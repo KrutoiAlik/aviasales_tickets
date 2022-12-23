4b8b9d93bef8c87f7b8274dcba0e6354cc920b94
Aleksei Bezmoshchuk <Aleksei_Bezmoshchuk@epam.com>
2022-12-23 23:08:25 +0300

    - added Inter fonts - removed unnecessary parts of CRA - added render of tickets - added filters by stops

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import './components/Checkbox/Checkbox.css';
+import logo from './airplane.png';
+import data from './tickets.json';
+import {Ticket, TicketCard} from "./components/TicketCard/TicketCard";
+
+const CURRENCY = new Map<string, string>([['RUB', '₽'], ['USD', '$'], ['EUR', '€']]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [currency, setCurrency] = useState<string>('RUB');
+    const [sortedBy, setSortedBy] = useState<string>('price'); // in case to sort tickets by other fields
+    const [stopFilters, setStopFilters] = useState<number[]>([]);
+
+    const sortTickets = (ticket1: Ticket, ticket2: Ticket) => {
+        const value1 = ticket1[sortedBy];
+        const value2 = ticket2[sortedBy];
+
+        if (value1 === value2) {
+            return 0;
+        }
+
+        if (value1 < value2) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    const tickets = !stopFilters.length
+        ? [...data.tickets]
+        : data.tickets.filter(ticket => stopFilters.includes(ticket.stops));
+
+    tickets.sort(sortTickets);
+    const stopsMessage = (stops: number) => {
+        if (stops === 1) {
+            return `${stops} пересадка`;
+        } else if (stops === 0) {
+            return 'Без пересадок';
+        } else if ([2, 3, 4].includes(stops % 10)) {
+            return `${stops} пересадки`;
+        }
+
+        return `${stops} пересадок`;
+    }
+
+    const updateStopFilters = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            setStopFilters(prev => [...prev, +e.target.value]);
+        } else {
+            setStopFilters(prev => prev.filter(filter => filter !== +e.target.value));
+        }
+    }
+
+    const getStopsOptions = () => {
+
+        const allStops = Array.from(new Set(data.tickets.map(ticket => ticket.stops))).sort();
+
+        const stopOptions: JSX.Element[] = [
+            <div className='stops-option'>
+                <input id='allStops'
+                       className='checkbox'
+                       checked={!stopFilters.length}
+                       type='checkbox'
+                       onChange={(e) => setStopFilters([])}/>
+                <label htmlFor='allStops'>Все</label>
+            </div>
+        ];
+
+        allStops.forEach(stops => {
+            stopOptions.push(<div className='stops-option'>
+                <input id={`stops_${stops}`}
+                       className='checkbox'
+                       value={stops}
+                       checked={stopFilters.includes(stops)}
+                       type='checkbox' onChange={updateStopFilters}/>
+                <label htmlFor={`stops_${stops}`}>{stopsMessage(stops)}</label>
+            </div>);
+        })
+
+        return stopOptions;
+    }
+
+    const getTicketElements = (): JSX.Element[] => {
+        return tickets.map((ticket, index) => (<TicketCard key={`ticket_${index}`}
+                                                           ticket={ticket}
+                                                           stops={stopsMessage(ticket.stops)}
+                                                           currency={CURRENCY.get(currency) || ''}/>));
+    }
+
+    const changeCurrency = (e: any) => {
+        if (currency === e.target.innerHTML) {
+            return;
+        }
+
+        setCurrency(e.target.innerHTML);
+    }
+
+    const getCurrencyButtons = () => {
+        return Array.from(CURRENCY.keys()).map(key => (
+                <button className={`currency-btn ${currency === key ? 'selected' : ''}`}
+                        onClick={changeCurrency}>{key}</button>
+            )
+        );
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} width='50' height='50' alt={'logo'}/>
+            </header>
+
+            <section className='App-content'>
+                <div className="options-sidebar">
+                    <div className="currency-options">
+                        <h3 className='sidebar-header'>Валюта</h3>
+                        <div className="currency-btn-group">
+                            {getCurrencyButtons()}
+                        </div>
+                    </div>
+                    <div className="stops-options">
+                        <h3 className='sidebar-header'>Количество пересадок</h3>
+                        <form>
+                            {getStopsOptions()}
+                        </form>
+                    </div>
+                </div>
+                <div className="tickets">
+                    {getTicketElements()}
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TicketCard/TicketCard.tsx
@@ -0,0 +1,78 @@
+import React, {FunctionComponent} from 'react';
+import './TicketCard.css';
+import logo from '../../turkish_airlines.png';
+
+interface IIndexAble {
+    [key: string]: string | number;
+}
+
+export type Ticket = IIndexAble & {
+    origin: string,
+    origin_name: string,
+    destination: string,
+    destination_name: string,
+    departure_date: string,
+    departure_time: string,
+    arrival_date: string,
+    arrival_time: string,
+    carrier: string,
+    stops: number,
+    price: number
+}
+
+type TicketCardProps = {
+    ticket: Ticket,
+    currency: string,
+    stops: string
+}
+
+export const TicketCard: FunctionComponent<TicketCardProps> = (props) => {
+
+    const dateString = (value: string) => {
+        let str = new Date(value).toLocaleDateString('ru-RU', {
+            weekday: 'short',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+        });
+
+        str = str[0].toUpperCase() + str.slice(1);
+
+        return str.replace(/\.|г/g, '').trim().split(', ').reverse().join(', ');
+    }
+
+    return (<div className="ticket">
+        <div className="ticket__purchase">
+            <div style={{paddingLeft: 10, paddingRight: 10}}>
+                <img src={logo} height={'40'} alt={'logo'}/>
+            </div>
+            <button className={'btn'}>Купить <br/>за {props.ticket.price}{props.currency}</button>
+        </div>
+        <div className="ticket__details">
+            <div className="ticket__duration">
+                <span className="ticket__time">{props.ticket.departure_time}</span>
+                <span className="ticket__time">{props.ticket.arrival_time}</span>
+
+                <span className="ticket__stops">
+                <span>{props.stops}</span>
+                    <div className='stops-line'></div>
+            </span>
+            </div>
+
+            <div className="destinations">
+                <div className="destination__details">
+                    <span className="destination__name">
+                        {props.ticket.origin}, {props.ticket.origin_name}
+                    </span>
+                    <span className='destination__date'>{dateString(props.ticket.departure_date)}</span>
+                </div>
+                <div className='destination__details text-align__right'>
+                    <span className="destination__name">
+                        {props.ticket.destination_name}, {props.ticket.destination}
+                    </span>
+                    <span className='destination__date'>{dateString(props.ticket.arrival_date)}</span>
+                </div>
+            </div>
+        </div>
+    </div>);
+}
